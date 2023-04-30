ff00834686b92ddb36a465c13901ae34adb32a60
Florent GATTI <florentgatti@macbook-pro-de-florent.home>
2023-04-30 19:10:07 +0200

    clean css inutile

--- WeFashion/resources/views/accueil.blade.php
+++ WeFashion/resources/views/accueil.blade.php
@@ -8,11 +8,6 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('resources/css/styleProduct.css') }}">
-
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/5f692740b2.js" crossorigin="anonymous"></script>
 </head>
